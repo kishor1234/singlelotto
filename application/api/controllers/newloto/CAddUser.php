d3f0862fc19df8c0466d49d1d21fde092c75be42
kishor shinde <kishor4shinde@gmail.com>
2022-12-10 09:12:59 +0530

    username prefix to userid

--- application/api/controllers/newloto/CAddUser.php
+++ application/api/controllers/newloto/CAddUser.php
@@ -591,7 +591,8 @@ class CAddUser extends CAaskController {
             if ($last_id > 250) {
                 array_push($error, "You Enter Only 25 id, try update limit...!");
             }
-            $userid = date("Y") . date("m") . date("d") + $last_id;
+            $name=str_replace(' ', '_', strtolower($data["name"]));
+            $userid = $name."".( date("Y") . date("m") . date("d") + $last_id);
             $sql = $this->ask_mysqli->update(array("userid" => $userid), "enduser") . $this->ask_mysqli->whereSingle(array("id" => $last_id));
             $this->adminDB[$_SESSION["db_1"]]->query($sql) != true ? array_push($error, $this->adminDB[$_SESSION["db_1"]]->error) : true;
             if (empty($error)) {
